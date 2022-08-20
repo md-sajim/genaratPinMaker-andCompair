98a2e8f62c124550b90abbbbd1d41b130df6147b
sajim <mdabutaliveb@gmail.com>
2022-08-20 21:44:59 +0600

    add wark number

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
-function genaratPin(){
-    const rendomNum = Math.round(Math.random()*10000);
+function genaratPin() {
+    const rendomNum = Math.round(Math.random() * 10000);
     return rendomNum;
 }
-function getPin(){
+function getPin() {
     const pin = genaratPin()
-    const pinString = pin+""
-    if(pinString.length === 4){
+    const pinString = pin + ""
+    if (pinString.length === 4) {
         return pin;
     }
-    else{
-       return getPin()
+    else {
+        return getPin()
     }
-     
+
 }
 
 
-document.getElementById('genarate-btn').addEventListener('click',function(){
-    
-   const getPinDigtes = getPin()
-const displayFile = document.getElementById("input-display");
-displayFile.value = getPinDigtes;
+document.getElementById('genarate-btn').addEventListener('click', function () {
+
+    const getPinDigtes = getPin()
+    const displayFile = document.getElementById("input-display");
+    displayFile.value = getPinDigtes;
+})
+document.getElementById('calc-body').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const displayFileCal = document.getElementById('displayFild-chack')
+    const prevaseValue = displayFileCal.value
+    if (isNaN(number)) {
+        if (number === 'C') {
+            displayFileCal.value = "";
+        }
+        else if (number === '<') {
+            const digits = prevaseValue.split('');
+            digits.pop()
+            const reminingDigits = digits.join('')
+            displayFileCal.value = reminingDigits;
+        }
+    }
+    else {
+        const prevaseNumAdd = prevaseValue + number
+        displayFileCal.value = prevaseNumAdd;
+    }
+
 })
